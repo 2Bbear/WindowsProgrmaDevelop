603ebd5f69009fc27edcc004f9495987f6974f23
2Bbear <wkdwjdgh1234@naver.com>
2019-02-18 17:14:30 +0900

    멀티미디어 적용 예제

---
+++ WPF/HowToWpf/WPFTutorials0218/WPFTutorials0218/MultiMedia/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTutorials0218.MultiMedia
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            myMedia.Volume = 10;
+            myMedia.Play();
+        }
+        void mediaPlay(Object sender, EventArgs e)
+        {
+            myMedia.Play();
+        }
+
+        void mediaPause(Object sender, EventArgs e)
+        {
+            myMedia.Pause();
+        }
+
+        void mediaMute(Object sender, EventArgs e)
+        {
+
+            if (myMedia.Volume == 100)
+            {
+                myMedia.Volume = 0;
+                muteButt.Content = "Listen";
+            }
+            else
+            {
+                myMedia.Volume = 100;
+                muteButt.Content = "Mute";
+            }
+        }
+    }
+}
