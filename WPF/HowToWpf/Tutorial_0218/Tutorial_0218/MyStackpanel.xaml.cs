7c5c75a9d5e556f0ad5549d8c7412e846da8fcfd
2Bbear <wkdwjdgh1234@naver.com>
2019-02-18 10:56:33 +0900

    WPF_기본사용법

---
+++ WPF/HowToWpf/Tutorial_0218/Tutorial_0218/MyStackpanel.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tutorial_0218
+{
+    /// <summary>
+    /// MyStackpanel.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MyStackpanel : Window
+    {
+        public MyStackpanel()
+        {
+            InitializeComponent();
+            //전체 패널
+            StackPanel stackPanel = new StackPanel();
+            this.Content = stackPanel;
+            //텍스트 블럭
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Welcome to XAML Tutorial this is changed";
+            textBlock.Height = 20;
+            textBlock.Width = 200;
+            textBlock.Margin = new Thickness(5);
+            stackPanel.Children.Add(textBlock);
+            //버튼
+            Button button = new Button();
+            button.Content = "OK this is changed";
+            button.Height = 20;
+            button.Width = 50;
+            button.Margin = new Thickness(20);
+            stackPanel.Children.Add(button);
+        }
+    }
+}
---
+++ WPF/HowToWpf/Tutorial_0218/Tutorial_0218/MyStackpanel2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tutorial_0218
+{
+    /// <summary>
+    /// MyStackpanel2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MyStackpanel2 : Window
+    {
+        public MyStackpanel2()
+        {
+            InitializeComponent();
+            button001.Content = "button001 changed";
+
+            //xaml을 거치지 않고 이벤트 바인딩
+            button002.Click +=this.Button001_customClick;
+
+
+        }
+        //단순 자동 생성으로 메소드 바인딩을 하였을 경우
+        private void Button001_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button001_Click ");
+        }
+
+        //커스텀 메소드 바인딩
+        private void Button001_customClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button001_customClick ");
+        }
+    }
+}
