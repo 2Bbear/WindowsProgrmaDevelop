fb684d21ddbfa6afe996c821f01ee79ed7a9c658
2Bbear <wkdwjdgh1234@naver.com>
2019-02-18 14:53:54 +0900

    WPF_ Dependency Property 예제

---
+++ WPF/HowToWpf/WPFTutorials0218/WPFTutorials0218/DependencyProperties/UserControl1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorials0218.DependencyProperties
+{
+    /// <summary>
+    /// UserControl1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SetTextProperty =
+           DependencyProperty.Register("SetText", typeof(string), typeof(UserControl1), new
+              PropertyMetadata("", new PropertyChangedCallback(OnSetTextChanged)));
+
+        public string SetText
+        {
+            get { return (string)GetValue(SetTextProperty); }
+            set { SetValue(SetTextProperty, value); }
+        }
+
+        private static void OnSetTextChanged(DependencyObject d,
+           DependencyPropertyChangedEventArgs e)
+        {
+            UserControl1 UserControl1Control = d as UserControl1;
+            UserControl1Control.OnSetTextChanged(e);
+        }
+
+        private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            tbTest.Text = e.NewValue.ToString();
+        }
+    }
+}
