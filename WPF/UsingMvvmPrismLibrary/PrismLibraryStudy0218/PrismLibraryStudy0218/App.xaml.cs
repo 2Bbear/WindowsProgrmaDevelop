db2484247f9883b33b06d0f51778680d0c8f5c45
2Bbear <wkdwjdgh1234@naver.com>
2019-02-18 22:04:08 +0900

    프리즘 라이브러리 - Bootstrapper 예제

---
+++ WPF/UsingMvvmPrismLibrary/PrismLibraryStudy0218/PrismLibraryStudy0218/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PrismLibraryStudy0218.MyBootstrap;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismLibraryStudy0218
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new MyBootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
