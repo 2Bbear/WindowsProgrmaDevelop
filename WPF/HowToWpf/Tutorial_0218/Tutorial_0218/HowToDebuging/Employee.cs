3df1f6c7c8a4035e20df5cb484a01d3c6f183adc
2Bbear <wkdwjdgh1234@naver.com>
2019-02-18 14:21:53 +0900

    XAML 정리 끝

---
+++ WPF/HowToWpf/Tutorial_0218/Tutorial_0218/HowToDebuging/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_0218.HowToDebuging
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+            var emp = new Employee()
+            {
+                Name = "Ali Ahmed",
+                Title = "Developer"
+            };
+            return emp;
+        }
+    }
+}
