10f7a9044dda696c766da5e8bc9b3b8726b7921c
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 08:17:38 +0900

    Prism

---
+++ WPF/UsingMvvmPrismLibrary/Prism_Samples/Prism_Samples/Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prism_Samples.Bootstrapper
+{
+    class Bootstrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
