1ab76555484a64a5423e1d03c668db9b224bb192
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 13:17:57 +0900

    ㅇ

---
+++ WPF/UsingMvvmPrismLibrary/07-Modules - AppConfig/Modules/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Modules.Views;
+using Prism.Modularity;
+using Prism.Unity;
+using Prism.Ioc;
+using System.Windows;
+
+namespace Modules
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
