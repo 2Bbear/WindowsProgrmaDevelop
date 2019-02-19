69173896927d6e747a214d470c4853b5adc0db66
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 17:01:03 +0900

    기본

---
+++ WPF/UsingMvvmPrismLibrary/ChangeConvention/ChangeConvention/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using ChangeConvention.Views;
+namespace ChangeConvention
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
