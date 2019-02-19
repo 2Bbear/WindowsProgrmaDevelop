d94d90861c4f5e4bada1ff7e16826ddc8723ebe6
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 11:32:38 +0900

    View에서 상호작용을 통한 View 출현방법

---
+++ WPF/UsingMvvmPrismLibrary/05-ViewInjection/ViewInjection/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using ViewInjection.Views;
+
+namespace ViewInjection
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ WPF/UsingMvvmPrismLibrary/05-ViewInjection/ViewInjection/Views/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+
+namespace ViewInjection.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IContainerExtension _container;
+        IRegionManager _regionManager;
+
+        public MainWindow(IContainerExtension container, IRegionManager regionManager)
+        {
+            InitializeComponent();
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        /*
+         어차피 regionmanager에 등록하는 과정이다. 바로 registerviewwithregion으로 하나
+         분리하여 등록 명 따로 붙일 view따로 하나 하는 행동은 같은 것이다.
+
+             */
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var view = _container.Resolve<ViewA>();
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+            //IRegion region = _regionManager.Regions["ContentRegion"];
+            //region.Add(view);
+        }
+    }
+}
