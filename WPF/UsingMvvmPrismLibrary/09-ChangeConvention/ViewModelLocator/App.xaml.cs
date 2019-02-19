154b07543f259a8b1d0f69d2e34ffa0a10360e69
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 16:52:59 +0900

    ㅇ
    ㅇ

---
+++ WPF/UsingMvvmPrismLibrary/09-ChangeConvention/ViewModelLocator/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Reflection;
+using System.Windows;
+using ViewModelLocator.Views;
+
+namespace ViewModelLocator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var viewModelName = $"{viewName}ViewModel, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+        }
+    }
+}
