f988c1ec7f1881cd7438c08de5873f40b394fc0c
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 10:59:55 +0900

    view에 view 호출하는 방법

---
+++ WPF/UsingMvvmPrismLibrary/04-ViewDiscovery/ViewDiscovery/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using ViewDiscovery.Views;
+
+namespace ViewDiscovery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
