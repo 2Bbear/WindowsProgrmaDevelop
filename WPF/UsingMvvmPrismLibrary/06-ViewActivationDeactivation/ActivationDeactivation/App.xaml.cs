d50aa3ff6921b240f6e4b3e1191c789c38540fa0
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 11:42:14 +0900

    버트으로 View전환

---
+++ WPF/UsingMvvmPrismLibrary/06-ViewActivationDeactivation/ActivationDeactivation/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ActivationDeactivation.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace ActivationDeactivation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
