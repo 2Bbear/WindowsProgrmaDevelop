8fd4f6be986a1bef7fb05908b57ec330d61ef028
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 10:38:45 +0900

    ㅇ
    ㅇ

--- WPF/UsingMvvmPrismLibrary/PrismLibraryStudy_Regions/PrismLibraryStudy_Regions/App.xaml.cs
+++ WPF/UsingMvvmPrismLibrary/PrismLibraryStudy_Regions/PrismLibraryStudy_Regions/App.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
+using Prism.Unity;
+
+using CommonServiceLocator;
+using PrismLibraryStudy_Regions.View;
 using System.Windows;
 
 namespace PrismLibraryStudy_Regions
@@ -13,5 +12,10 @@ namespace PrismLibraryStudy_Regions
     /// </summary>
     public partial class App : Application
     {
+        protected Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+        
     }
 }
