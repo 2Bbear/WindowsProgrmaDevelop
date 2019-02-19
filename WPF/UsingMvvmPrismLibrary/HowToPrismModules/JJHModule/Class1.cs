a9b41149e770d3574e05e89b4ba6bab64d0ebd7c
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 15:50:40 +0900

    2
    2

--- WPF/UsingMvvmPrismLibrary/HowToPrismModules/JJHModule/Class1.cs
+++ WPF/UsingMvvmPrismLibrary/HowToPrismModules/JJHModule/Class1.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
 namespace JJHModule
 {
-    public class Class1
+    public class Class1 : IModule
     {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<RegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion",typeof(Views.ViewA));
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
