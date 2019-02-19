15aa52cb56ad69dce57ea849ae39cf48f5055df4
2Bbear <wkdwjdgh1234@naver.com>
2019-02-19 15:46:07 +0900

    모듈기초

---
+++ WPF/UsingMvvmPrismLibrary/HowToPrismModules/HowToPrismModules/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using HowToPrismModules.Views;
+namespace HowToPrismModules
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
