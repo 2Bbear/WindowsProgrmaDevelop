2520e6aef3d56e152af85903b7a31ec440b73f30
2Bbear <wkdwjdgh1234@naver.com>
2019-02-27 17:09:00 +0900

    MvvM을 구현한 프로젝트

---
+++ WPF/UsingDevExpress/EasyHandleDevExProject/EasyHandleDevExProject/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Core;
+
+namespace EasyHandleDevExProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
